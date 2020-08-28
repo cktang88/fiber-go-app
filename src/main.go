07f696e65b0333310c340b61eccda1d29f7769e3
cktang88 <arctangent20@yahoo.com>
2020-08-27 22:50:14 -0500

    refactor

--- src/main.go
+++ src/main.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"time"
 
 	"github.com/dgrijalva/jwt-go"
 	dbr "github.com/gocraft/dbr/v2"
@@ -13,8 +11,6 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 
 	jwtware "github.com/gofiber/jwt"
-
-	"github.com/RediSearch/redisearch-go/redisearch"
 )
 
 func main() {
@@ -80,35 +76,6 @@ func main() {
 	app.Listen(3000)
 }
 
-func login(c *fiber.Ctx) {
-	user := c.FormValue("user")
-	pass := c.FormValue("pass")
-
-	// Throws Unauthorized error
-	if user != "john" || pass != "doe" {
-		c.SendStatus(fiber.StatusUnauthorized)
-		return
-	}
-
-	// Create token
-	token := jwt.New(jwt.SigningMethodHS256)
-
-	// Set claims
-	claims := token.Claims.(jwt.MapClaims)
-	claims["name"] = "John Doe"
-	claims["admin"] = true
-	claims["exp"] = time.Now().Add(time.Hour * 72).Unix()
-
-	// Generate encoded token and send it as response.
-	t, err := token.SignedString([]byte("secret"))
-	if err != nil {
-		c.SendStatus(fiber.StatusInternalServerError)
-		return
-	}
-
-	c.JSON(fiber.Map{"token": t})
-}
-
 func accessible(c *fiber.Ctx) {
 	c.Send("Accessible")
 }
@@ -118,46 +85,4 @@ func restricted(c *fiber.Ctx) {
 	claims := user.Claims.(jwt.MapClaims)
 	name := claims["name"].(string)
 	c.Send("Welcome " + name)
-}
-
-func redisInit() {
-
-	// Create a client. By default a client is schemaless
-	// unless a schema is provided when creating the index
-	c := redisearch.NewClient("localhost:6379", "myIndex")
-
-	// Create a schema
-	sc := redisearch.NewSchema(redisearch.DefaultOptions).
-		AddField(redisearch.NewTextField("body")).
-		AddField(redisearch.NewTextFieldOptions("title", redisearch.TextFieldOptions{Weight: 5.0, Sortable: true})).
-		AddField(redisearch.NewNumericField("date"))
-
-	// Drop an existing index. If the index does not exist an error is returned
-	// c.Drop()
-
-	// Create the index with the given schema
-	if err := c.CreateIndex(sc); err != nil {
-		log.Fatal(err)
-	}
-
-	// Create a document with an id and given score
-	doc := redisearch.NewDocument("doc1", 1.0)
-	doc.Set("title", "Hello world").
-		Set("body", "foo bar").
-		Set("date", time.Now().Unix())
-}
-func searchText(c *redisearch.Client, text string) {
-
-	// Index the document. The API accepts multiple documents at a time
-	if err := c.Index([]redisearch.Document{doc}...); err != nil {
-		log.Fatal(err)
-	}
-
-	// Searching with limit and sorting
-	docs, total, err := c.Search(redisearch.NewQuery("hello world").
-		Limit(0, 2).
-		SetReturnFields("title"))
-
-	fmt.Println(docs[0].Id, docs[0].Properties["title"], total, err)
-	// Output: doc1 Hello world 1 <nil>
 }
